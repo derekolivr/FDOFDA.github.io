72955f1a4284fb68313d04139e417c33c2f68bc4
Derek <derekoliverf@gmail.com>
2024-04-25 11:48:27 +0530

    script-add

---
+++ script1.js
@@ -0,0 +1,22 @@
+const getTodos = (resource, callback) => {
+    const request = new XMLHttpRequest();
+
+    request.addEventListener('readystatechange', () =>{
+        //console.log(request, request.readyState);
+    
+        if (request.readyState === 4 && request.status === 200) {
+            const data = JSON.parse(request.responseText)
+            callback(undefined, data);
+        } else if (request.readyState === 4) {
+            callback('could not fetch the data', undefined);
+        }
+    });
+    
+    
+    request.open('GET', resource);
+    request.send();
+}
+
+getTodos('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&month=2009-01&outputsize=full&apikey=demo', (err, data) => {
+    console.log(data);
+});
