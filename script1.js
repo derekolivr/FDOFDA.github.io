097e529fb1f04a7f6a1c69e08420020fa476656e
Derek <derekoliverf@gmail.com>
2024-04-27 00:22:59 +0530

    get-data

--- script1.js
+++ script1.js
@@ -16,11 +16,22 @@ const stockchart = candlechart.addCandlestickSeries({
     
 });
 
-fetch('https://.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&month=2009-01&outputsize=full&apikey=demo').then((response) => {
+fetch('https://alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&month=2009-01&outputsize=full&apikey=demo').then((response) => {
     console.log('success', response);
-    return response.json;
-}).then((data) => {
-    console.log(data);
+    return response.json();
+}).then(data => {
+    const timeSeries = data['Time Series (5min)'];
+    const chartData = Object.keys(timeSeries).map((time) => {
+        return {
+            time: time,
+            open: parseFloat(timeSeries[time]['1. open']),
+            high: parseFloat(timeSeries[time]['2. high']),
+            low: parseFloat(timeSeries[time]['3. low']),
+            close: parseFloat(timeSeries[time]['4. close']),
+        };
+    });
+
+    stockchart.setData(chartData);
 }).catch((error) => {
     console.log('failed', error);
 })
@@ -28,18 +39,7 @@ fetch('https://.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&i
 
 
 
-stockchart.setData([
-    { time: '2018-12-22', open: 75.16, high: 82.84, low: 36.16, close: 45.72 },
-    { time: '2018-12-23', open: 45.12, high: 53.90, low: 45.12, close: 48.09 },
-    { time: '2018-12-24', open: 60.71, high: 60.71, low: 53.39, close: 59.29 },
-    { time: '2018-12-25', open: 68.26, high: 68.26, low: 59.04, close: 60.50 },
-    { time: '2018-12-26', open: 67.71, high: 105.85, low: 66.67, close: 91.04 },
-    { time: '2018-12-27', open: 91.04, high: 121.40, low: 82.70, close: 111.40 },
-    { time: '2018-12-28', open: 111.51, high: 142.83, low: 103.34, close: 131.25 },
-    { time: '2018-12-29', open: 131.33, high: 151.17, low: 77.68, close: 96.43 },
-    { time: '2018-12-30', open: 106.33, high: 110.20, low: 90.39, close: 98.10 },
-    { time: '2018-12-31', open: 109.87, high: 114.69, low: 85.66, close: 111.26 },
-]);
+
 
 
 
@@ -63,14 +63,6 @@ stockchart.setData([
         if (request.readyState === 4 && request.status === 200) {
             const data = JSON.parse(request.responseText);
             callback(null, data);
-
-
-
-
-
-
-
-
 
         } else if (request.readyState === 4) {
             callback('Could not fetch the data', null);
