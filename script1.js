396d750d8ea611537f57bc26b9d8c4aea2429b97
Derek <derekoliverf@gmail.com>
2024-04-27 16:47:36 +0530

    Create-workingmodel

--- script1.js
+++ script1.js
@@ -4,80 +4,37 @@ const candlechart = LightweightCharts.createChart(container, {
         textColor: '#bbbbbb',
         background: {color: '#222'}
     }
-
 });
 
 const stockchart = candlechart.addCandlestickSeries({
-    upColor: '#26a69a', 
-    downColor: '#ef5350', 
+    upColor: '#26a69a',
+    downColor: '#ef5350',
     borderVisible: true,
-    wickUpColor: '#26a69a', 
+    wickUpColor: '#26a69a',
     wickDownColor: '#ef5350',
-    
 });
 
-fetch('https://alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&month=2009-01&outputsize=full&apikey=demo').then((response) => {
-    console.log('success', response);
-    return response.json();
-}).then(data => {
-    const timeSeries = data['Time Series (5min)'];
-    const chartData = Object.keys(timeSeries).map((time) => {
-        return {
-            time: time,
-            open: parseFloat(timeSeries[time]['1. open']),
-            high: parseFloat(timeSeries[time]['2. high']),
-            low: parseFloat(timeSeries[time]['3. low']),
-            close: parseFloat(timeSeries[time]['4. close']),
-        };
+fetch('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&outputsize=full&apikey=demo')
+    .then((response) => response.json())
+    .then(data => {
+        const timeSeries = data['Time Series (5min)'];
+        const chartData = Object.keys(timeSeries)
+            .map((dateTime) => {
+                const date = new Date(dateTime); // Create a Date object from the datetime string
+                const unixTimestamp = Math.floor(date.getTime() / 1000);
+
+                return {
+                    time: unixTimestamp,
+                    open: parseFloat(timeSeries[dateTime]['1. open']),
+                    high: parseFloat(timeSeries[dateTime]['2. high']),
+                    low: parseFloat(timeSeries[dateTime]['3. low']),
+                    close: parseFloat(timeSeries[dateTime]['4. close']),
+                };
+            })
+            .sort((a, b) => a.time - b.time); // Sort data points by time in ascending order
+
+        stockchart.setData(chartData);
+    })
+    .catch((error) => {
+        console.log('Error:', error);
     });
-
-    stockchart.setData(chartData);
-}).catch((error) => {
-    console.log('failed', error);
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* const getTodos = (resource, callback) => {
-    const request = new XMLHttpRequest();
-
-    request.addEventListener('readystatechange', () =>{
-        if (request.readyState === 4 && request.status === 200) {
-            const data = JSON.parse(request.responseText);
-            callback(null, data);
-
-        } else if (request.readyState === 4) {
-            callback('Could not fetch the data', null);
-        }
-    });
-    
-    request.open('GET', resource);
-    request.send();
-}
-
-getTodos('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&month=2009-01&outputsize=full&apikey=demo', (err, data) => {
-    if (err) {
-        console.error('Error:', err);
-    } else {
-        console.log('Data:', data);
-    }
-});
-*/
